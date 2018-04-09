bd6dd556e635bbcdfd191b5147ad87a54b67c4b2
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2018-04-09 15:26:44 +0200

    déplacement vers le bas dans le cas ou un ascenceur est présent

--- jeux/scripts/app.js
+++ jeux/scripts/app.js
@@ -1,39 +1,52 @@
-let perso = document.createElement('div')
+let character = document.createElement('div')
 let lane = document.querySelectorAll('div[class^=lane]')
 let left = 10
+let topPos = 10
+let random = Math.floor(Math.random()*5)
+let compteur = 1
 
 init()
 
 function init(){
-  perso.style.width = '2%'
-  perso.style.height = '4%'
-  perso.style.backgroundColor = 'red'
-  perso.style.position = 'absolute'
-  perso.style.left = left + 'px'
+  //on initialise le jeux
+  createCharacter()
 
-  lane[(Math.floor(Math.random()*5))].appendChild(perso)
-
-  game()
 }
 
 
   window.addEventListener('keydown', function(e){
+    //on gère les déplacements du personnage en fonction de la touche
     e.preventDefault()
-    switch (e.keyCode) {
-      case 39:
+      let corridor = character.parentNode.nextSibling.nextSibling
+      if (e.keyCode==39)
+      {
+        //déplace le personnage vers la droite
         left += 10
-        perso.style.left = left + 'px'
-        break;
-      case 37:
+        character.style.left = left + 'px'
+      }
+      if (e.keyCode==37 && left > 0)
+      {
+        //déplace le personnage vers la gauche
         left -= 10
-        perso.style.left = left + 'px'
-        break;
-    }
+        character.style.left = left + 'px'
+      }
+      if (e.keyCode==40 && (corridor.offsetLeft - 20) <= left && (corridor.offsetLeft + corridor.offsetWidth)>=(left + character.offsetWidth))
+      {
+        //déplace le personnage à la ligne inférieur en le supprimant puis en le recréant à la ligne inférieur
+         character.parentNode.removeChild(character)
+         lane[random+compteur].appendChild(character)
+         compteur += 1
+      }
   })
 
-  function game(){
-  //pour lancer la fonction tous les 40millisecondes
-  let gamePlay = setInterval(
-    function(){
-    },40)
+function createCharacter(){
+  //on le personnage et on le place sur le terrain
+  character.style.width = '1%'
+  character.style.height = '50%'
+  character.style.backgroundColor = 'red'
+  character.style.position = 'absolute'
+  character.style.left = left + 'px'
+  character.style.top = topPos + 'px'
+
+  lane[random].appendChild(character)
 }
