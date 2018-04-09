97952049deaa4dec4570d6b3432f24c4c7af5f95
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2018-04-09 15:56:38 +0200

    déplacement haut

--- jeux/scripts/app.js
+++ jeux/scripts/app.js
@@ -2,8 +2,7 @@ let character = document.createElement('div')
 let lane = document.querySelectorAll('div[class^=lane]')
 let left = 10
 let topPos = 10
-let random = Math.floor(Math.random()*5)
-let compteur = 1
+let place = Math.floor(Math.random()*5)
 
 init()
 
@@ -17,7 +16,8 @@ function init(){
   window.addEventListener('keydown', function(e){
     //on gère les déplacements du personnage en fonction de la touche
     e.preventDefault()
-      let corridor = character.parentNode.nextSibling.nextSibling
+      let nextCorridor = character.parentNode.nextSibling.nextSibling
+      let pastCorridor = character.parentNode.previousSibling.previousSibling
       if (e.keyCode==39)
       {
         //déplace le personnage vers la droite
@@ -30,12 +30,19 @@ function init(){
         left -= 10
         character.style.left = left + 'px'
       }
-      if (e.keyCode==40 && (corridor.offsetLeft - 20) <= left && (corridor.offsetLeft + corridor.offsetWidth)>=(left + character.offsetWidth))
+      if (e.keyCode==40 && (nextCorridor.offsetLeft - 20) <= left && (nextCorridor.offsetLeft + nextCorridor.offsetWidth)>=(left + character.offsetWidth))
       {
-        //déplace le personnage à la ligne inférieur en le supprimant puis en le recréant à la ligne inférieur
+        //déplace le personnage à la ligne inférieur en le supprimant puis en le recréant à la ligne inférieur si un ascenceur est présent
          character.parentNode.removeChild(character)
-         lane[random+compteur].appendChild(character)
-         compteur += 1
+         place += 1
+         lane[place].appendChild(character)
+      }
+      if (e.keyCode==38 && (pastCorridor.offsetLeft - 20) <= left && (pastCorridor.offsetLeft + pastCorridor.offsetWidth)>=(left + character.offsetWidth))
+      {
+        //déplace le personnage à la ligne supérieur en le supprimant puis en le recréant à la ligne inférieur si un ascenceur est présent
+        character.parentNode.removeChild(character)
+        place -= 1
+        lane[place].appendChild(character)
       }
   })
 
@@ -48,5 +55,5 @@ function createCharacter(){
   character.style.left = left + 'px'
   character.style.top = topPos + 'px'
 
-  lane[random].appendChild(character)
+  lane[place].appendChild(character)
 }
