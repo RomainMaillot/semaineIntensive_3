a8578f53926df65412c8b23ab2d4df6102cb9b2d
AymericArn <32864333+rodolphus@users.noreply.github.com>
2018-04-10 15:10:35 +0200

    génération aléatoire fonctionnelle

--- jeux/scripts/app.js
+++ jeux/scripts/app.js
@@ -2,7 +2,7 @@ let character = document.createElement('div')
 let lane = document.querySelectorAll('div[class^=lane]')
 let left = 10
 let mapWrapper = document.querySelector('.map')
-let place = Math.floor(Math.random()*5)
+let place = Math.floor(Math.random()*4)
 
 init()
 
@@ -14,14 +14,58 @@ function init(){
 }
 
 function initMap() {
+
+    //Ajoute des noeuds html correspondant
+
+    let oldCorrs = document.querySelectorAll('div[class^=corridor]')
+
+    let lanes = document.querySelectorAll('div[class^=lane]')
+    
+    let corrCount = 1 //numéro du couloir
+    let laneCorrCount = [0, 0, 0] // compteur des couloirs entre deux lane
+
+    for (let i = 0; i < oldCorrs.length; i++) {
+        oldCorrs[i].parentNode.removeChild(oldCorrs[i])
+    }
+    for (let j = 0; j < 3; j++) {
+        
+        for (let i = 0; i < Math.floor(Math.random() * 3 + 1); i++) {
+            let newCorr = document.createElement('div')
+            newCorr.classList.add('corridor' + corrCount)
+
+            lanes[j].after(newCorr)
+            corrCount++
+            laneCorrCount[j]++
+        }
+    }
+
+    console.log(laneCorrCount)
+
+    let corrPositions = [[], [], []]
+    
+    for (let j = 0; j < 3; j++) { //boucle qui parcours chaque le premier niveau du tableau
+        for (let k = 0; k < laneCorrCount[j]; k++) { //boucle qui ajoute le bon nombre de positions
+            let randInt = Math.floor(Math.random() * 60)
+            // if (randInt - corrPositions[j][k - 1] > 10 && (randInt - corrPositions[j - 1][k] > 10 || corrPositions[j - 1][k] - randInt > 10)) {
+            //     console.log('suuss la sossiss')
+            // }
+            corrPositions[j].push(randInt)       
+        }
+        corrPositions[j].sort(function (a, b) {return a - b})
+    }
+
+    console.log(corrPositions)
+
     let map = "" //chaîne de caractère contenant la disposition du terrain
 
-    let fullLane, laneNum = 1, emptyLane = "", corNum = 1
+    let fullLane, laneNum = 1, emptyLane = "", corrNum = 0, k = 0
 
-    //remplissage de la string
-    for (let i = 0; i < 4; i++) {
+    //REMPLISSAGE DE LA CHAINE //
+
+    //ne doit pas changer
+    for (let i = 0; i < 3; i++) {
         fullLane = ""
-        emptyLane = ""
+        emptyLane = "" 
         for (let j = 0; j < 60; j++) {
             fullLane += `lane${laneNum} `
         }
@@ -29,19 +73,41 @@ function initMap() {
         map += '\n'
         laneNum++
 
-        for (let j = 0; j < 60; j++) {
-            let randomChara = Math.floor(Math.random() * 31)
-            
-            if ((randomChara >= 29 || emptyLane.substring(emptyLane.length - 16, emptyLane.length + 1) === ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ") && emptyLane.substring(0, emptyLane.length).includes(`corridor${i * 2}`) === false ) {
-                emptyLane += `corridor${corNum}`
-                corNum++
-            } else  {
+        //crée et remplit un tableau avec des positions aléatoires pour les couloirs
+
+        for (let j = 0; j < 60; j++) { //parcours la ligne
+            if (j === corrPositions[i][k]) {
+                emptyLane += `corridor${corrNum + 1}`
+                corrNum++
+                k++
+            } else {
                 emptyLane += '. '
             }
         }
+
+        k = 0
         map += `"${emptyLane}"`
         map += '\n'
-        console.log(emptyLane.substring(emptyLane.length - 16, emptyLane.length + 1))
+
+        // for (let j = 0; j < corrCount[i]; j++) { //permet de fouttre le bon nombre de corridor par lane dans le css
+        //     for (let k = 0; k < 60; k++) {
+                                
+        //     }            
+        // }
+
+        // for (let j = 0; j < 60; j++) {
+        //     let randomChara = Math.floor(Math.random() * 31)
+            
+        //     if ((randomChara >= 29 || emptyLane.substring(emptyLane.length - 16, emptyLane.length + 1) === ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ") && emptyLane.substring(0, emptyLane.length).includes(`corridor${i * 2}`) === false ) {
+        //         emptyLane += `corridor${corrNum + 1}`
+        //         corrNum++
+        //     } else  {
+        //         emptyLane += '. '
+        //     }
+        // }
+        // map += `"${emptyLane}"`
+        // map += '\n'
+        // console.log(emptyLane.substring(emptyLane.length - 16, emptyLane.length + 1))
     }
 
     fullLane = ""
@@ -53,6 +119,7 @@ function initMap() {
     console.log(map)
 
     $('.map').css('grid-template-areas', map)
+
 }
 
 function listenArrows() {
@@ -89,6 +156,18 @@ function listenArrows() {
         }
         windowMove()
     })
+}
+
+function setElevators() {
+    let elevators = document.querySelectorAll('div[class^=corridor]')
+
+    for (let i = 0; i < elevators.length; i++) {
+        for (let j = 0; j < 2; j++) {
+            let elevator = document.createElement('img')
+            elevator.setAttribute('src', 'images/elevator_door.png')
+            elevators[i].appendChild(elevator) 
+        }
+    }
 }
 
 function windowMove () {
