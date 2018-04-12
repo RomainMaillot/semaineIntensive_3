89dfb703d16031db32f6f7e446c4005d56c03c3f
AymericArn <32864333+rodolphus@users.noreply.github.com>
2018-04-12 20:26:59 +0200

    Page d'accueil OK avec choix du mode

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 let container = document.querySelector('.container')
+let playerModes = document.querySelectorAll('.mode_select a')
 let site2 = document.querySelector('.site2')
-let button = document.querySelector('a')
+let button = document.querySelector('.box')
+localStorage.setItem('gameMode', '0')
 
 button.addEventListener('click', (e) =>  {
   e.preventDefault()
@@ -15,3 +17,12 @@ button.addEventListener('click', (e) => {
   }, 285)
 
 })
+
+for (let i = 0; i < playerModes.length; i++) {
+  playerModes[i].addEventListener('click', (e) => {
+    e.preventDefault()
+    e.currentTarget.classList.add('active')
+    localStorage.setItem('gameMode', i)
+    playerModes[Math.abs(i - 1)].classList.remove('active')
+  })
+}
